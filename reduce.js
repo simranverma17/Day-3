0d865483f54922225eda6c1afe5be0ce801d1628
Simran Verma <simranverma.sv17@gmail.com>
2025-07-10 20:04:22 +0530

    Items are fixed.

--- reduce.js
+++ reduce.js
@@ -1,9 +1,9 @@
 const cart = [
     {item: "Hershiee's" ,price: 450},//objects
     {item: "Lays" ,price: 50},
-    {items: "Maggie" ,price: 160},
-    {items: "Almonds" ,price: 650},
-    {items: "Cashew" ,price: 350},
+    {item: "Maggie" ,price: 160},
+    {item: "Almonds" ,price: 650},
+    {item: "Cashew" ,price: 350},
 ];
 const total = cart.reduce((sum, product)=> sum + product.price, 0);
 console.log("Total:", total);
